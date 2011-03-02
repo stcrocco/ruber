8917dde3054b69072ec36617c1f390fdbea6bc02
Stefano Crocco <stefano.crocco@alice.it>
2011-03-02 13:43:53 +0100

    Added World::DocumentFactory class
    This class takes care of ensuring that only one document exists for any
    given file

---
+++ lib/ruber/world/document_factory.rb
@@ -0,0 +1,118 @@
+=begin 
+    Copyright (C) 2011 by Stefano Crocco   
+    stefano.crocco@alice.it   
+  
+    This program is free software; you can redistribute it andor modify  
+    it under the terms of the GNU General Public License as published by  
+    the Free Software Foundation; either version 2 of the License, or     
+    (at your option) any later version.                                   
+  
+    This program is distributed in the hope that it will be useful,       
+    but WITHOUT ANY WARRANTY; without even the implied warranty of        
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+    GNU General Public License for more details.                          
+  
+    You should have received a copy of the GNU General Public License     
+    along with this program; if not, write to the                         
+    Free Software Foundation, Inc.,                                       
+    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             
+=end
+
+require 'ruber/editor/document'
+
+module Ruber
+  
+  module World
+  
+=begin rdoc
+Factory class to create documents
+
+It ensures that at all times there's only a single document associated with a
+given file.
+
+@note in the documentation, whenever a file name is mentioned, it can be replaced
+  by a @KDE::Url@.
+=end
+    class DocumentFactory < Qt::Object
+      
+=begin rdoc
+@param [Qt::Object,nil] parent the parent object
+=end
+      def initialize parent = nil
+        super
+        @documents = {}
+      end
+    
+=begin rdoc
+Returns a document, creating it if needed
+
+If a file name is specified, a document for that file will be returned.
+If a document for that file name already exists, it will be returned. Otherwise, a new
+document will be created.
+
+If no file or URL is given, a new document (not associated with a file) is returned.
+@param [String,KDE::Url,nil] file if not nil, the absolute name or the URL of the
+  file to retrieve the document for. If *nil*, a new document not associated with
+  any file will be created
+@param [Qt::Object,nil] parent the object the document should be child of. If *nil*,
+  the document will be parentless
+@return [Document,nil] a document associated with _file_ or a new document not
+  associated with a file if _file_ is *nil*. If _file_ represents a local file
+  and that file doesn't exist, *nil* is returned
+=end
+      def document file, parent = nil
+        if file
+          url = KDE::Url.new file
+          return if url.local_file? and !File.exist?(url.path)
+          doc = @documents[url]
+          doc || create_document(file, parent)
+        else create_document nil, parent
+        end
+      end
+      
+      private
+      
+=begin rdoc
+Slot called whenever a document is closed
+
+It ensures that the document is removed from the internal document list
+@param [Document] doc the document being closed
+=end
+      def document_closed doc
+        @documents.delete doc.url
+      end
+      slots 'document_closed(QObject*)'
+      
+=begin rdoc
+Slot called whenever a document's URL changes
+
+It updates the internal list of documents
+@param [Document] doc the document whose URL has changed
+=end
+      def document_url_changed doc
+        @documents.reject!{|k, v| v == doc}
+        @documents[doc.url] = doc
+      end
+      slots 'document_url_changed(QObject*)'
+      
+=begin rdoc
+Creates a new document
+
+After creating the document, makes all the necessary signal-slot connections with
+it and adds it to the internal document list if needed
+@param (see Ruber::World::DocumentFactory#document)
+@return [Ruber::Document] the new document
+=end
+      def create_document file, parent
+        doc = Document.new file, parent
+        connect doc, SIGNAL('closing(QObject*)'), self, SLOT('document_closed(QObject*)')
+        connect doc, SIGNAL('document_url_changed(QObject*)'), self, SLOT('document_url_changed(QObject*)')
+        @documents[doc.url] = doc if file
+        doc
+      end
+      
+    end
+    
+  end
+  
+end
---
+++ spec/document_factory_spec.rb
@@ -0,0 +1,97 @@
+require './spec/framework'
+require './spec/common'
+require 'ruber/kernel/document_factory'
+
+describe Ruber::World::DocumentFactory do
+  
+  it 'derives from Qt::Object' do
+    Ruber::World::DocumentFactory.ancestors.should include(Qt::Object)
+  end
+  
+  before do
+    @factory = Ruber::World::DocumentFactory.new
+  end
+  
+  describe '#document' do
+    
+    it 'returns a document having the object passed as second argument as parent' do
+      obj = Qt::Object.new
+      doc = @factory.document nil, obj
+      doc.should be_a(Ruber::Document)
+      doc.parent.should == obj
+    end
+    
+    context 'when called with a file or URL as first argument' do
+      
+      it 'returns a new document for the given file, if no other document for it exists' do
+        doc = @factory.document __FILE__
+        doc.path.should == __FILE__
+      end
+      
+      it 'returns an existing document for the same file, if it already exists' do
+        old = @factory.document __FILE__
+        new = @factory.document __FILE__
+        new.should == old
+      end
+      
+      it 'returns the existing file even if of the two calls to document one was passed a string and the other an URL' do
+        old = @factory.document KDE::Url.new(__FILE__)
+        new = @factory.document __FILE__
+        new.should == old
+      end
+      
+      it 'does not attempt to return a document which has been closed' do
+        old = @factory.document __FILE__
+        old.close
+        new = @factory.document __FILE__
+        new.should_not == old
+      end
+      
+      it 'returns an existing document even if it was created without a file and saved with the new name later' do
+        old = @factory.document nil
+        url = KDE::Url.new __FILE__
+        flexmock(old).should_receive(:url).and_return url
+        old.instance_eval{emit document_url_changed(self)}
+        new = @factory.document __FILE__
+        new.should == old
+      end
+      
+      it 'returns an existing document even if it was created for another file then
+      saved with the new name later' do
+        old = @factory.document File.join File.dirname(__FILE__), 'common.rb'
+        url = KDE::Url.new __FILE__
+        flexmock(old).should_receive(:url).and_return url
+        old.instance_eval{emit document_url_changed(self)}
+        new = @factory.document __FILE__
+        new.should == old
+      end
+      
+      it 'doesn\'t return a document which was created for the same file but was saved with another name later' do
+        old = @factory.document __FILE__ 
+        url = KDE::Url.new File.join(File.dirname(__FILE__), 'common.rb')
+        flexmock(old).should_receive(:url).and_return url
+        old.instance_eval{emit document_url_changed(self)}
+        new = @factory.document __FILE__
+        new.should_not == old
+      end
+      
+      it 'returns nil if the file is a local file and it doesn\'t exist' do
+        @factory.document('/xyz').should be_nil
+        @factory.document(KDE::Url.new('/xyz')).should be_nil
+      end
+      
+    end
+    
+    context 'when called with nil as first argument' do
+      
+      it 'always returns a new document' do
+        old = @factory.document nil
+        new = @factory.document nil
+        new.should_not == old
+      end
+      
+    end
+    
+  end
+  
+end
