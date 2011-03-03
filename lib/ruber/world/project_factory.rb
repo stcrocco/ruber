0b97e3bf5ccd2e4bc61991f327abad97163c6358
Stefano Crocco <stefano.crocco@alice.it>
2011-03-03 13:53:48 +0100

    Added World::ProjectFactory class
    The task of this class is to make sure there's only a single project
    object for a given project file

---
+++ lib/ruber/world/project_factory.rb
@@ -0,0 +1,119 @@
+=begin 
+    Copyright (C) 2011 by Stefano Crocco   
+    stefano.crocco@alice.it   
+  
+    This program is free software; you can redistribute it andor modify  
+    it under the terms of the GNU General Public License as published by  
+    the Free Software Foundation; either version 2 of the License, or     
+    (at your option) any later version.                                   
+  
+    This program is distributed in the hope that it will be useful,       
+    but WITHOUT ANY WARRANTY; without even the implied warranty of        
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+    GNU General Public License for more details.                          
+  
+    You should have received a copy of the GNU General Public License     
+    along with this program; if not, write to the                         
+    Free Software Foundation, Inc.,                                       
+    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             
+=end
+
+module Ruber
+  
+  module World
+    
+=begin rdoc
+Class whose task is to ensure that there's only one project open for any given
+project file.
+
+To create a new project, call the {#project} method instead of using {Project.new}.
+If a project for the given file already exists, it'll be returned, otherwise a
+new project will be created.
+=end
+    class ProjectFactory < Qt::Object
+      
+=begin rdoc
+Exception raised when the name requested for a project file is different for the
+name contained in the project itself
+=end
+      class MismatchingNameError < StandardError
+        
+=begin rdoc
+@return [String] the project file
+=end
+        attr_reader :file
+        
+=begin rdoc
+@return [String] the requested project name
+=end
+        attr_reader :requested_name
+        
+=begin rdoc
+@return [String] the project name contained in the project file
+=end
+        attr_reader :actual_name
+        
+=begin rdoc
+@param [String] file the project file
+@param [String] requested_name the name requested for the project
+@param [String] actual_name the project name contained in the project
+=end
+        def initialize file, requested_name, actual_name
+          @file = file
+          @requested_name = requested_name
+          @actual_name = actual_name
+          super "A project associated with #{file} exists, but the corresponding project name is #{actual_name} instead of #{requested_name}"
+        end
+      end
+      
+=begin rdoc
+@param [Qt::Object,nil] parent the parent object
+=end
+      def initialize parent = nil
+        super
+        @projects = {}
+      end
+      
+=begin rdoc
+Retrieves the project associated with a given project file
+
+If a project associated with the project file _file_ already exists, that project
+is returned. Otherwise, a new project is created.
+
+@param (see Ruber::Project#initialize)
+@return [Project] a project associated with _file_
+@raise [MismatchingNameError] if _name_ is specified, a project associated with
+  _file_ already exists but _name_ and the name of the existing project are different
+=end
+      def project file, name = nil
+        prj = @projects[file]
+        if prj
+          if name and prj.project_name != name
+            raise MismatchingNameError.new file, name, prj.project_name
+          end
+          prj
+        else
+          prj = Project.new file, name
+          connect prj, SIGNAL('closing(QObject*)'), self, SLOT('project_closing(QObject*)')
+          @projects[prj.project_file] = prj
+        end
+      end
+      
+      private
+      
+=begin rdoc
+Method called whenever a project is closed
+
+It ensures that the list of open projects is up to date
+@return [nil]
+=end
+      def project_closing prj
+        @projects.delete prj.project_file
+        nil
+      end
+      slots 'project_closing(QObject*)'
+      
+    end
+  end
+  
+end
---
+++ spec/project_factory_spec.rb
@@ -0,0 +1,86 @@
+require 'spec/framework'
+require './spec/common'
+require 'ruber/world/project_factory'
+
+require 'tempfile'
+require 'tmpdir'
+
+describe Ruber::World::ProjectFactory do
+  
+  before do
+    @factory = Ruber::World::ProjectFactory.new
+  end
+  
+  describe '#project' do
+    
+    context 'when called with a file name as argument' do
+      
+      before do
+        @file = Tempfile.new ['project_factory_test', '.ruprj']
+        @file.write YAML.dump({:general => {:project_name => 'project_factory_test'}})
+        @file.flush
+      end
+      
+      after do
+        @file.close!
+      end
+      
+      it 'returns a new project associated with the file if no other project is associated with it' do
+        prj = @factory.project @file.path
+        prj.should be_a(Ruber::Project)
+        prj.project_file.should == @file.path
+      end
+      
+      it 'returns an existing project associated with the same file, if that project exists' do
+        old = @factory.project @file.path
+        new = @factory.project @file.path
+        new.should == old
+      end
+      
+      it 'doesn\'t return an existing project which has been closed' do
+        old = @factory.project @file.path
+        old.close
+        new = @factory.project @file.path
+        new.should_not == old
+      end
+      
+    end
+    
+  end
+  
+  context 'when called with a file name and a project name as arguments' do
+    
+    it 'returns a new project associated with the file if no other project is associated with it' do
+      file = File.join Dir.tmpdir, 'project_factory_test.ruprj'
+      prj = @factory.project file, 'project_factory_test'
+      prj.should be_a(Ruber::Project)
+      prj.project_file.should == file
+    end
+    
+    it 'returns the existing project associated with the file if there is such a project and the name of the project is equal to the second argument' do
+      file = File.join Dir.tmpdir, 'project_factory_test.ruprj'
+      old = @factory.project file, 'project_factory_test'
+      prj = @factory.project file, 'project_factory_test'
+      prj.should == old
+    end
+    
+    it 'raises ProjectFactory::MismatchingNameError if there\'s a project associated with the same file but the project name is different from the second argument' do
+      file = File.join Dir.tmpdir, 'project_factory_test.ruprj'
+      old_name = 'project_factory_test'
+      old = @factory.project file, old_name
+      new_name = 'project_factory_test_other_name'
+      lambda{@factory.project file, new_name}.should raise_error(Ruber::World::ProjectFactory::MismatchingNameError, "A project associated with #{file} exists, but the corresponding project name is #{old_name} instead of #{new_name}")
+    end
+    
+    it 'doesn\'t return an existing project which has been closed' do
+      file = File.join Dir.tmpdir, 'project_factory_test.ruprj'
+      old = @factory.project file, 'project_factory_test'
+      old.close false
+      new = @factory.project file, 'project_factory_test'
+      new.should_not == old
+    end
+
+    
+  end
+  
+end
