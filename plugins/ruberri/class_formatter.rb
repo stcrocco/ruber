524f5c82f5a5c5c677cc1bc377bb09282f919e2e
Stefano Crocco <stefano.crocco@alice.it>
2015-03-25 15:58:37 +0100

    Added documentation for plugins/ruberri/class_formatter.rb

--- plugins/ruberri/class_formatter.rb
+++ plugins/ruberri/class_formatter.rb
@@ -28,17 +28,70 @@ module Ruber
 
   module RI
 
+=begin rdoc
+Class with the task of producing an HTML page from the @RI@ documentation for a
+class or module.
+
+The HTML page is generated using an ERB template contained in the @class.rhtml@
+file.
+=end
     class ClassFormatter
 
      Data = Struct.new :title, :type, :name, :inheritance, :included, :comment,
          :constants, :class_methods, :instance_methods, :store, :store_type
 
+=begin rdoc
+Helper class providing access to information provided by @RI@ from the ERB template.
+
+@!attribute title
+  @return [String] the title of the page
+@!attribute type
+  @return [String] the type of entity (@module@ or @class@)
+@!attribute name
+  @return [String] the name of the class
+@!attribute inheritance
+ @return [String] the name of the base class or an empty string if the base
+    class is @Object@
+@!attribute included
+  @return [<String>] a list of all included modules
+@!attribute comment
+  @return [String] the description associated with the class
+@!attribute constants
+  @return [{String => String}] the names of the constants defined in the class
+    together with their descriptions
+@!attribute class_methods
+  @return [<RDoc::AnyMethod>] a list of class methods
+@!attribute instance_methods
+  @return [<RDoc::AnyMethod>] a list of instance methods
+@!attribute store
+  @return [String] the path to the store file
+@!attribute store_type
+  @return [String] the type of store
+
+@!method initialize
+  Returns a new instance of Data
+
+  All attributes are set to @nil@
+  @return [Data]
+=end
      class Data
+
+=begin rdoc
+The @Binding@ associated with the instance
+
+@return [Binding] the @Binding@ associated with the instance. This is needed
+  by ERB
+=end
        def get_binding
          binding
        end
      end
 
+=begin rdoc
+@param [String] cls the full name of the class
+@param [String] store the path of the file containing the store
+@param [String] store_type the type of the store
+=end
       def initialize cls, store, store_type
         @store = RDoc::RI::Store.new store, store_type
         @cls = @store.load_class cls
@@ -49,6 +102,11 @@ module Ruber
         @data.store = store
       end
 
+=begin rdoc
+Generates the HTML page for the class
+
+@return [String] the HTML code of the page
+=end
       def to_html
         superclass = @cls.superclass if @cls.type == :class
         if superclass and superclass != 'Object'
@@ -79,6 +137,11 @@ module Ruber
 
       private
 
+=begin rdoc
+Generates HTML code from the RDoc-formatted documentation
+
+@param [RDoc::Markup::Document] doc the document to format
+=end
       def format_document doc
         doc.accept @formatter
       end
